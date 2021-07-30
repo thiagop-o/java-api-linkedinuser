1dceef0e01cf0fce0d27b1dc113f12a2d7983e34
Thiago Oliveira <thiagop_o@hotmail.com>
2021-07-30 15:59:35 -0300

    Subindo arquivos iniciais do Spring Boot

---
+++ src/main/java/com/linkedin/apilinkedin/ApilinkedinApplication.java
@@ -0,0 +1,13 @@
+package com.linkedin.apilinkedin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApilinkedinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApilinkedinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linkedin/apilinkedin/ApilinkedinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkedin.apilinkedin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApilinkedinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
